ca800d20929ff7a6bacc5b3ae5d1a8694a552a96
PLAYER <angeltrinidad5758@gmail.com>
2025-03-19 11:23:33 -0400

    Add files via upload

--- scrips.js
+++ scrips.js
@@ -1,17 +1,21 @@
-const billInput = document.getElementById('bill');
-const peopleInput = document.getElementById('people');
-const tipButtons = document.querySelectorAll('.tip');
-const customTipInput = document.getElementById('custom-tip');
-const tipAmountDisplay = document.getElementById('tip-amount');
-const totalAmountDisplay = document.getElementById('total-amount');
-const resetButton = document.getElementById('reset');
-const input = document.getElementById('people');
-const errorMessage = document.getElementById('error-message');
+let bill = document.querySelector('#bill');
+let billNumber = parseInt(bill.value);
 
-people.addEventListener('people', function () {
-    if (input.value === '0') {
-        hidden.style.display = 'block';
-    } else {
-        hidden.style.display = 'none';
-    }
-});
+let people = document.querySelector('#people');
+let peopleNumber = parseInt(people.value);
+
+let tipResult = document.querySelector('#tip-amount');
+let totalResult = document.querySelector('#total-amount')
+
+let buttons = document.querySelectorAll('.tip-btn');
+
+buttons.forEach(element => {
+    element.addEventListener('click', event=>{
+        let tipValue =parseInt(event.target.innerText.slice(0,-1));
+        //calculo de tip amount
+        tipResult.innerText = ((billNumber * tipValue / 100) / peopleNumber);
+    
+        //calculo del total
+        totalResult.innerText = ((billNumber * tipResult / 100) + billNumber)/peopleNumber;
+    });
+})
